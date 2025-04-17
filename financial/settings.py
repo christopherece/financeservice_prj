4b8d7cc02d5825b392b0a1a0a3643b7235e59156
christopherece <christopheranchetaece@gmail.com>
2025-04-17 13:58:07 +1200

    Added services2

--- financial/settings.py
+++ financial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g)q2)cya)me34x-1_@fp*9#g+)oo768hlp-ru9-jj==^9^b$ki
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.10.250', 'localhost', 'financeservice.balaydalakay.com']
+ALLOWED_HOSTS = ['192.168.10.250', 'localhost', 'financeservice.balaydalakay.com','financeservice.topitsolutions.co.nz']
 
 
 # Application definition
